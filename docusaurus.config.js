eb2e50dd8556088bf286f7c7444953d5f37890c6
Ubuntu <ubuntu@ip-10-0-10-198.eu-central-1.compute.internal>
2020-11-20 14:48:22 +0100

    cambio dominio

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'My Site',
   tagline: 'The tagline of my site',
-  url: 'https://DevBlinders.github.io',
+  url: 'https://docs.devblinders.com',
   baseUrl: '/devblinders-kb/',
   onBrokenLinks: 'throw',
   favicon: 'img/favicon.ico',
