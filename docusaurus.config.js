86c7390685450b1923415faf562947b4fd6953a7
DevBlinders <62160017+DevBlinders@users.noreply.github.com>
2021-01-05 09:37:46 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,52 +27,7 @@ module.exports = {
     },
     footer: {
       style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Style Guide',
-              to: 'docs/',
-            },
-            {
-              label: 'Second Doc',
-              to: 'docs/doc2/',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: 'blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} Documentación DevBlinders | Todos los derechos reservados`,
+      copyright: `Copyright © ${new Date().getFullYear()} - <a href="https://devblinders.com/es/aviso-legal/">Aviso legal y Protección de datos</a> - <a href="https://devblinders.com/es/politica-de-cookies/">Política de Cookies</a> - <a href="https://www.youtube.com/channel/UCO_DK9hrDZ-WXYK8sbtu3ig" target="_blank" rel="me" title="Canal de eCommerce y SEO de DevBlinders">DevBlinders en YouTube</a>`,
     },
   },
   presets: [
